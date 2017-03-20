a56365b20dd6037aa5117c3fc54168b1a874bda6
jaggesher <jaggesher14@gmail.com>
2017-03-20 20:52:10 +0600

    Updating Outer section filed, of main.blade.php

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,12 +3,16 @@
   <head>
     {{--Every child page should inject page title through section name title--}}
     @include('partials.MainPartials._head')
+    @yield('OuterInclude')
   </head>
 
   <body>
      @include('partials.MainPartials._navigation')
       <div id="ContentOfBody" class="container-fluid">
-        <h1><center>Here we Just add Our Content</center></h1>
+
+        @yield('ContentOfBody')
+       {{-- <h1><center>Here we Just add Our Content</center></h1> --}} 
+
       </div>
   </body>
 </html>
