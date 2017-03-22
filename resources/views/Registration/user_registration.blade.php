74fcb949d4e1c992e686c315090ecf974ede059d
Shoukhin <shoukhin1993@gmail.com>
2017-03-22 07:58:04 +0600

    updating registration form

--- resources/views/Registration/user_registration.blade.php
+++ resources/views/Registration/user_registration.blade.php
@@ -1,44 +1,57 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    {{--Every child page should inject page title through section name title--}}
-    @include('partials.MainPartials._head')
-    @section('title', '| Registration') <!-- title isn't working-->
-  </head>
+@extends('main')
 
-  <body>
-     @include('partials.MainPartials._navigation')
-      <div id="registrationBody" class="container-fluid">
-        <h1><center>Basic registration form</center></h1>
-      </div>
+@section('title')
+	Registration
+@endsection 
 
-   <div class="container-fluid">
+@section('OuterInclude')
+	<style type="text/css"></style>
+@endsection
+
+
+ @section('ContentOfBody')
+
+<div class = "registrationBody">
+  <div class="row">
    <div class="col-sm-8"> </div>
    <div class="col-sm-3">   
-	<form>
-	<div class="form-group">
-	    <input type="text" class="form-control" id="userFirstName" placeholder="First Name">
-	  </div>
-	  <div class="form-group">
-	    <input type="text" class="form-control" id="userLastName" placeholder="Last Name">
-	  </div>
-	<div class="form-group">
-	    <input type="text" class="form-control" id="userID"  placeholder="Enter Student ID">
-	  </div>
-	  <div class="form-group">
-	    <input type="password" class="form-control" id="pwd" placeholder="password">
-	  </div>
-	  <div class="form-group">
-	    <input type="password" class="form-control" id="cpwd" placeholder="Re-type password">
-	  </div>
-	  <div class="form-group">
-	    <input type="email" class="form-control" id="email" placeholder="Enter Email">
-	  </div>
-	  	  
-	  <button type="submit" class="btn btn-primary btn-block">Submit</button>
-	</form>
+	  <form>
+		<div class="form-group">
+		    <input type="text" class="form-control" id="userFirstName" placeholder="First Name">
+		 </div>
+		  <div class="form-group">
+		    <input type="text" class="form-control" id="userLastName" placeholder="Last Name">
+		  </div>
+		<div class="form-group">
+		    <input type="text" class="form-control" id="userID"  placeholder="Enter Student ID">
+		  </div>
+		  <div class="form-group">
+		    <input type="password" class="form-control" id="pwd" placeholder="password">
+		  </div>
+		  <div class="form-group">
+		    <input type="password" class="form-control" id="cpwd" placeholder="Re-type password">
+		  </div>
+		  <div class="form-group">
+		    <input type="email" class="form-control" id="email" placeholder="Enter Email">
+		  </div>
+		  <div class="form-group">
+		    <input type="text" class="form-control" id="userContactNo" placeholder="Contact No.">
+		  </div>
+			<div class="form-group">
+			<label for="male">Gender : </label>
+		  <label class="radio-inline">
+	           <input type="radio" name="male">Male
+	      </label>
+	    <label class="radio-inline">
+	      		<input type="radio" name="female">Female
+	    </label>
+	    </div>
+		  	  
+		  <button type="submit" class="btn btn-primary btn-block">Submit</button>
+		</form>
 
 	</div> 
-	</div> <!-- container-fluid-->
-  </body>
-</html>
+	</div>
+	</div>
+@endsection
+  
