4dc05241e4e876e5ad8194abab5669ec190c439d
IAmMonmoy <iammonmoy@gmail.com>
2017-03-23 01:40:37 +0600

    added image for homepage

---
+++ resources/views/Home/home.blade.php
@@ -0,0 +1,25 @@
+@extends('main')
+
+@section('title')
+	Home
+@endsection
+
+@section('ContentOfBody')
+<div class="container-fluid">
+    <div class="row">
+        <div class="home_head col-bg-12">
+            <img src="/image/home1.png" alt="HomePage Image" class="img-responsive">
+            <h1>Welcome To Contest Registration</h1>
+            <p >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        
+        <div class="col-md-12 body_content">
+            <a href="">
+                <h1>There are not contest right now</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </a>
+        </div>
+    </div>
+</div>
+    
+@endsection
