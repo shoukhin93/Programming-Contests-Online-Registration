591d5c62965446394a427b0a0d5e428218acdb2c
jaggesher <jaggesher14@gmail.com>
2017-03-23 12:42:50 +0600

    Personal Info Update Prototype

--- resources/views/profile/profileUpdate.blade.php
+++ resources/views/profile/profileUpdate.blade.php
@@ -19,40 +19,87 @@
     <h2>Update Profie</h2>
   </div>
 
-  <div class="col-sm-6">
-    <form>
-        <div class="form-group form-inline">
-          <label for="usrID"><strong>User ID:</strong></label>
-          <input type="text" class="form-control" id="usrID">
+  <div class="col-sm-4" align="right">
+    <img src="{{ asset('image/RakulPreet.jpg') }}" class="img-thumbnail" alt="Profile Pic" width="200" height="200">
+  </div>
+
+  <div class="col-sm-8 pro_info">
+  <h3>Update Your Personal info</h3>
+    <form class="form-horizontal" role="form">
+
+      <div class="form-group">
+        <label class="col-lg-3 control-label">Your ID:</label>
+        <div class="col-lg-8">
+          <input class="form-control" type="text" value="14025423" readonly>
         </div>
-        <div class="form-group form-inline">
-          <label for="FirstName">First Name:</label>
-          <input type="text" class="form-control" id="FirstName" placeholder="Rakul Preet">
-        </div>
-        <div class="form-group form-inline">
-          <label for="LastName">Last Name:</label>
-          <input type="text" class="form-control" id="LastName" placeholder="Sing">
-        </div>
-        <div class="form-group form-inline">
-          <label for="year">Your Year:</label>
-          <select id="year" name="studentYear" class="form-control">
-            <option style="color: black" value="1">1st Year</option>
-            <option style="color: black" value="2">2nd Year</option>
-            <option style="color: black" value="3" selected>3rd Year</option>
-            <option style="color: black" value="4">4th Year</option>
-          </select>
-      </div>
-      <div class="form-group form-inline">
-        <label for="email"> Your Email:</label>
-        <input type="email" class="form-control" id="email" placeholder="info@something.com">
       </div>
 
+      <div class="form-group">
+        <label class="col-lg-3 control-label">First name:</label>
+        <div class="col-lg-8">
+          <input class="form-control" type="text" value="Rakul Preet">
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label class="col-lg-3 control-label">Last name:</label>
+        <div class="col-lg-8">
+          <input class="form-control" type="text" value="Sing">
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label class="col-lg-3 control-label">Email:</label>
+        <div class="col-lg-8">
+          <input class="form-control" type="email" value="info@something.com">
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label class="col-lg-3 control-label">Your Year:</label>
+        <div class="col-lg-8">
+          <div class="ui-select">
+            <select id="user_time_zone" class="form-control">
+              <option style="color: black" value="1">1st Year</option>
+              <option style="color: black" value="2">2nd Year</option>
+              <option style="color: black" value="3" selected>3rd Year</option>
+              <option style="color: black" value="4">4th Year</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label class="col-lg-3 control-label">Contact No:</label>
+        <div class="col-lg-8">
+          <input class="form-control" type="text" value="01915770274">
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label class="col-md-3 control-label">Password:</label>
+        <div class="col-md-8">
+          <input class="form-control" type="password" value="" placeholder="Enter New Password">
+        </div>
+      </div>
+      <div class="form-group">
+        <label class="col-md-3 control-label">Confirm password:</label>
+        <div class="col-md-8">
+          <input class="form-control" type="password" value="" placeholder="Confirm New Password">
+        </div>
+      </div>
+
+      <div class="form-group">
+        <label class="col-md-3 control-label"></label>
+        <div class="col-md-8">
+          <input type="button" class="btn btn-primary" value="Update Profile">
+          <span></span>
+          {{-- <input type="reset" class="btn btn-default" value="Cancel"> --}}
+        </div>
+      </div>
     </form>
   </div>
 
-  <div class="col-sm-6">
-    
-  </div>
 </div>
 @endsection 
 
