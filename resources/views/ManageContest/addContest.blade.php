f275f8c2c9bee2779f02fa806ec1f4c9f8a80c9d
Shoukhin <shoukhin1993@gmail.com>
2017-03-23 20:32:05 +0600

    updating final version of add contest

--- resources/views/ManageContest/addContest.blade.php
+++ resources/views/ManageContest/addContest.blade.php
@@ -5,42 +5,65 @@
 @endsection 
 
 @section('OuterInclude')
+	
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.3.0/js/bootstrap-datepicker.js"></script>
 	<link href="{{ asset('css/user_registration.css') }}" rel="stylesheet" >
+
 @endsection
 
 @section('ContentOfBody')
 
+<div class="container">
 <div class="registrationBody">
 	<div class="row">
-		<h1 align="center"> Arrange Contest </h1>
-		<p align="center">note: no more than one contest can be arrange at a time</p>
+	<div class="col-sm-8"></div>
+		<div class=" col-sm-4">
+		<div class="head">
+	    <h2 align="center">Contest Arrangement</h2>
+	  </div>
+	  </div>
 		
 	</div>
 
 	<div class="row">
-		<div class="col-sm-4"></div>
+
+
+		   	<div class="col-sm-6"> 
+			   		<div class="jumbotron">
+				    <h2 align="center">Arrange contest by filling the form</h2>  <br>   				    
+					<p>No more than one running  contest registration process can be arranged at a time</p><br>
+					<p align="center">Happy Coding! :)</p>
+				</div>
+			</div>
+
+		<div class="col-sm-2"></div>
 
 		<div class="col-sm-4">
 
-		<form>
+		<form >
 			<div class="form-group">
-				<textarea class="form-control" rows="5" id="comment" placeholder="Description"></textarea>
+				<label for="date">Enter Date & time:</label>
+				<input class="form-control" type="datetime-local"" name="date" id="date" placeholder="Enter Date" >
+			</div>	
+
+
+
+			<div class="form-group">
+				
+				<textarea class="form-control" rows="10" id="description" placeholder="description(optional)"></textarea>
 			</div>
 			<div class="form-group">
-				<button type="submit" class="btn btn-primary btn-block">Submit</button>
+				<button type="submit" class="btn btn-primary btn-block">Add Contest</button>
 			</div>
 		</form>
 			
 		</div>
 
-		<div class="col-sm-4">
-			
-		</div>
 		
 	</div>
 	
 </div>
-
+</div>
 
 
 
