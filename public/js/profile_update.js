bcab485aa3994a22fc4d5596b39394cf42a7c4f4
jaggesher <jaggesher14@gmail.com>
2017-03-24 00:47:40 +0600

    Adding some Jqury For profile edit

--- public/js/profile_update.js
+++ public/js/profile_update.js
@@ -1,10 +1,8 @@
 // Here Goes Javascript & jqury for profileUpdate.blade.php
 
 $(document).ready(function(){
-	// alert();
 	$(document).on('click', '.browse', function(){
 	      var file = $(this).parent().parent().parent().find('.file');
 	      file.trigger('click');
-	      
 	    });
 });
