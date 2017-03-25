26507fbb4cef79e0172ff843aa787a2c496fe7b5
jaggesher <jaggesher14@gmail.com>
2017-03-25 16:09:47 +0600

    Fixing Style Of Member Request

--- resources/views/Registration/member_request.blade.php
+++ resources/views/Registration/member_request.blade.php
@@ -16,17 +16,17 @@
 {{-- Main Member Request View --}}
 <div class="container">
 	<div class="row">
-		<div class="col-sm-2"></div>
-		<div class="col-sm-8 mem_head clearfix">
+		{{-- <div class="col-sm-2"></div> --}}
+		<div class="col-sm-12 mem_head clearfix">
 			<h2>MEMBER REQUEST</h2>
 			<p>Thse all members are requested to join. Once you if approve them, then they will enjoy the functionality.</p>
 		</div>
-		<div class="col-sm-2"></div>
+		{{-- <div class="col-sm-2"></div> --}}
 	</div>
 	<div class="row">
 		<div class="col-sm-3">
 			<div class="mem_box">
-				<h2><strong>ID:</strong>14025423</h2>
+				<h2><strong> <i>14025423</i></strong></h2>
 				<h3>Rakul Preet Sing</h3>
 				<h4>Third Year Student.</h4>
 				<h4><span class="glyphicon glyphicon-phone-alt"></span <i> : 01915770274</i></h4>
