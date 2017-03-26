5f9ffcba11b1aace8926a46da67b7b4e175365b7
Shoukhin <shoukhin1993@gmail.com>
2017-03-26 18:27:18 +0600

    linked with contest registration

--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -32,7 +32,7 @@
                 fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
                 deserunt mollit anim id est laborum.</p>
 
-            <a href="user_registration" class="btn btn-success" role="button">Click For Registration</a>
+            <a href="contest_registration" class="btn btn-success" role="button">Click For Registration</a>
         </div>
     </div>
 
