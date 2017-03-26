93638705b6ded1a65087a31cda973c7955991a11
Shoukhin <shoukhin1993@gmail.com>
2017-03-26 18:25:26 +0600

    linked home with user registration

--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -18,19 +18,21 @@
                 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                 ex ea commodo consequat.</p>
             <!--<img src="/image/home1.png" alt="HomePage Image" class="img-responsive"> -->
-            <button class="btn btn-danger">Click For More Details</button>
+
+            <a href="#contestInformation" class="btn btn-danger" role="button">Click For More Details</a>
         </div>
     </div>
 
     <div class="wrapper">
-        <div class="contest-information">
+        <div class="contest-information" id = "contestInformation">
             <h1>Contest information</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
                 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
                 ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
                 fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
                 deserunt mollit anim id est laborum.</p>
-            <button class="btn btn-success">Click For Registraion</button>
+
+            <a href="user_registration" class="btn btn-success" role="button">Click For Registration</a>
         </div>
     </div>
 
