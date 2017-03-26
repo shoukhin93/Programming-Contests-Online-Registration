00d5e4788ab63df927d145902782070e4e37dfa5
jaggesher <jaggesher14@gmail.com>
2017-03-26 23:52:34 +0600

    Fixing Responsive Problems of contests

--- resources/views/Lists/contests.blade.php
+++ resources/views/Lists/contests.blade.php
@@ -22,19 +22,27 @@
 	<div class="list-group">
 		<a href="#" class="list-group-item">
 			<h3 class="list-group-item-heading">Rajshahi University Inter Dept Programming Contest</h3>
-			<p class="list-group-item-text">26-Mar-2017 11.30 AM</p>
-		</a>
-		<a href="#" class="list-group-item">
-			<h3 class="list-group-item-heading">Rajshahi University Inter Dept Programming Contest</h3>
-			<p class="list-group-item-text">26-Mar-2017 11.30 AM</p>
-		</a>
-		<a href="#" class="list-group-item">
-			<h3 class="list-group-item-heading">Rajshahi University Inter Dept Programming Contest</h3>
-			<p class="list-group-item-text">26-Mar-2017 11.30 AM</p>
-		</a>
-		<a href="#" class="list-group-item">
-			<h3 class="list-group-item-heading">Rajshahi University Inter Dept Programming Contest</h3>
 			<span class="pull-right label label-success">Running..</span>
+			<p class="list-group-item-text">26-Mar-2017 11.30 AM</p>
+		</a>
+		<a href="#" class="list-group-item">
+			<h3 class="list-group-item-heading">Rajshahi University Inter Dept Programming Contest 01</h3>
+			<span class="pull-right label label-info">Finished</span>
+			<p class="list-group-item-text">26-Mar-2017 11.30 AM</p>
+		</a>
+		<a href="#" class="list-group-item">
+			<h3 class="list-group-item-heading">Rajshahi University Inter Dept Programming Contest 02</h3>
+			<span class="pull-right label label-info">Finished</span>
+			<p class="list-group-item-text">26-Mar-2017 11.30 AM</p>
+		</a>
+		<a href="#" class="list-group-item">
+			<h3 class="list-group-item-heading">Rajshahi University Team Selection Contest 01</h3>
+			<span class="pull-right label label-info">Finished</span>
+			<p class="list-group-item-text">26-Mar-2017 11.30 AM</p>
+		</a>
+		<a href="#" class="list-group-item">
+			<h3 class="list-group-item-heading">Rajshahi University Inter Dept Programming Contest</h3>
+			<span class="pull-right label label-info">Finished</span>
 			<p class="list-group-item-text">26-Mar-2017 11.30 AM</p>
 		</a>
 		<a href="#" class="list-group-item">
