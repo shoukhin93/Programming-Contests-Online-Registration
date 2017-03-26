5034988c4a493c6e9642c98e4092294d5d030fa9
IAmMonmoy <iammonmoy@gmail.com>
2017-03-26 22:51:12 +0600

    adding jquery in home.js

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,22 @@
 $("document").ready(function(){
      $("nav").addClass("navbar-fixed-top");
      $("div#ContentOfBody").removeClass("container-fluid");
+     var test = $(window).scrollTop();
+     $(document).scroll(function(){
+        var v = $(window).scrollTop();
+        if(v>90 && v>test){
+            $(".add").text("ScrollTop "+v);
+            $(".cover-text").css("display","block");
+            
+        }
+        else if(v<=90)
+        {
+            $(".cover-text").css("display","table-cell");
+        }
+        else{
+
+        }
+        test = v;
+     });
+    
 });
