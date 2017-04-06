5bee338a649028e2963925e78cedd2c6f65edc5c
jaggesher <jaggesher14@gmail.com>
2017-04-07 02:13:24 +0600

    Adding Team Rank Migration

--- database/migrations/2017_04_06_183611_Create_Team_Result_Table.php
+++ database/migrations/2017_04_06_183611_Create_Team_Result_Table.php
@@ -13,10 +13,23 @@ class CreateTeamResultTable extends Migration
      */
     public function up()
     {
-        // Schema::create('team_results', function (Blueprint $table) {
-        //     $table->increments('id');
-        //     $table->timestamps();
-        // });
+        Schema::create('team_results', function (Blueprint $table) {
+            $table->integer('contest_id')->primary();
+            $table->string('team_name',20)->nullable(false);
+            $table->string('mem1',8)->nullable(false);
+            $table->string('mem2',8)->nullable(false);
+            $table->string('mem3',8)->nullable(false);
+            $table->integer('problem_solved')->nullable(false);
+            $table->integer('time_penalty')->nullable(false);
+            $table->integer('rank')->nullable(false);
+
+            $table->timestamps();
+
+            $table->foreign('contest_id')->references('contest_id')->on('previous_contests');
+            $table->foreign('mem1')->references('student_id')->on('accounts');
+            $table->foreign('mem2')->references('student_id')->on('accounts');
+            $table->foreign('mem3')->references('student_id')->on('accounts');
+        });
     }
 
     /**
