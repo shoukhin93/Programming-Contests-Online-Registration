efb7d0a3634cecc8e73ecb8866fa4680b46dadb9
jaggesher <jaggesher14@gmail.com>
2017-04-17 01:20:38 +0600

    Adding All Model

---
+++ app/individual_result.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class individual_result extends Model
+{
+    //
+}
---
+++ app/previous_contest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class previous_contest extends Model
+{
+    //
+}
