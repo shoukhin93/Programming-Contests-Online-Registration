700f06e7bcf5aae5ab4c1f864a9c66dccc731098
jaggesher <jaggesher14@gmail.com>
2017-05-05 16:46:18 +0600

    Adding Admin Migration

---
+++ database/migrations/2017_05_05_104155_create_admin_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('admin_id',20)->uniqid()->nullable(false);
+            $table->string('name')->nullable(false);
+            $table->string('password')->nullable(false);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
